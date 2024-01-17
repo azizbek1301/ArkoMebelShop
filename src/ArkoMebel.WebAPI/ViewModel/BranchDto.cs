c9583c344d437ceabf0e9f12f459a93eefd15b9a
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-17 14:11:03 +0500

    BranchDto

---
+++ src/ArkoMebel.WebAPI/ViewModel/BranchDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ArkoMebel.WebAPI.ViewModel
+{
+    public class BranchDto
+    {
+        public string Name { get; set; }
+        public int OpenAt { get; set; }
+        public int CloseAt { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int AddressId { get; set; }
+        public string Link { get; set; } = default!;
+    }
+}
