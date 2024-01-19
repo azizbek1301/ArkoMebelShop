b574beb07ae2094b8827a20bb4fae69e18cea9c6
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-19 16:45:17 +0500

    reklama osti component

---
+++ ArkoMebelFront/src/app/pages/reklama-osti/reklama-osti.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reklama-osti',
+  standalone: true,
+  imports: [],
+  templateUrl: './reklama-osti.component.html',
+  styleUrl: './reklama-osti.component.css'
+})
+export class ReklamaOstiComponent {
+
+}
