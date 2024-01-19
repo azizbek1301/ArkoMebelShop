fc014038e7cc0708089f416576e5d055e9cccdcb
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-19 16:45:06 +0500

    reklama component

---
+++ ArkoMebelFront/src/app/pages/reklama/reklama.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reklama',
+  standalone: true,
+  imports: [],
+  templateUrl: './reklama.component.html',
+  styleUrl: './reklama.component.css'
+})
+export class ReklamaComponent {
+
+}
