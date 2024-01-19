0def6846306f1fef4c49e73ddbb31b25ba0c9289
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-19 16:45:32 +0500

    Arko mebel Front

--- ArkoMebelFront/src/app/app.component.ts
+++ ArkoMebelFront/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { KuhnniComponent } from './pages/kuhnni/kuhnni.component';
+import { ReklamaComponent } from './pages/reklama/reklama.component';
+import { ReklamaOstiComponent } from './pages/reklama-osti/reklama-osti.component';
+import { KatalogComponent } from './pages/katalog/katalog.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,KuhnniComponent,ReklamaComponent,ReklamaOstiComponent,
+    KatalogComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ArkoMebelFront/src/app/app.routes.ts
+++ ArkoMebelFront/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { KuhnniComponent } from './pages/kuhnni/kuhnni.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "navbar", component: NavbarComponent},
+    {path:"kuhnni",component:KuhnniComponent}
+];
