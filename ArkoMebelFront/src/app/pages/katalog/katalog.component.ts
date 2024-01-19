6258fb68902604c3e9b5ff26346a4cc6bf653bbe
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-19 16:43:49 +0500

    katalog component

---
+++ ArkoMebelFront/src/app/pages/katalog/katalog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-katalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './katalog.component.html',
+  styleUrl: './katalog.component.css'
+})
+export class KatalogComponent {
+
+}
