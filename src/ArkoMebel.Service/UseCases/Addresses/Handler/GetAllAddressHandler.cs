2fe3f5d754c36ca13ad9d29795584f236b750aca
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-21 17:08:05 +0500

    AddressHandlaer

--- src/ArkoMebel.Service/UseCases/Addresses/Handler/GetAllAddressHandler.cs
+++ src/ArkoMebel.Service/UseCases/Addresses/Handler/GetAllAddressHandler.cs
@@ -1,5 +1,5 @@
 ﻿using ArkoMebel.Domain.Entites;
-using ArkoMebel.Service.Abstraction;
+using ArkoMebel.Service.Abstraction.DataAccess;
 using ArkoMebel.Service.UseCases.Addresses.Queries;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
