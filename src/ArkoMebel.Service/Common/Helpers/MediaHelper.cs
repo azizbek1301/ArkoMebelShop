f6f7bb24c3549ffe152f8eb2fa7ced03790964a8
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-21 17:07:51 +0500

    File Services

---
+++ src/ArkoMebel.Service/Common/Helpers/MediaHelper.cs
@@ -0,0 +1,36 @@
+﻿using ArkoMebel.Domain.Exceptions.NotValid;
+
+namespace ArkoMebel.Service.Common.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string fileName)
+        {
+            var fileInfo= new FileInfo(fileName);
+            var imageExtensions = GetImageExtensions();
+
+            if(imageExtensions.Any(x=>x==fileInfo.Extension))
+            {
+                string extensions = fileInfo.Extension;
+                string name = "IMG_" + Guid.NewGuid() + extensions;
+                return name;
+            }
+            throw new Exception("Image Not Found");
+           
+        }
+
+
+
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+                ".jpg", 
+                ".jpeg",
+                ".png",
+                ".bmp",
+                ".svg"
+            };
+        }
+    }
+}
---
+++ src/ArkoMebel.Service/Service/Files/FileService.cs
@@ -0,0 +1,54 @@
+﻿using ArkoMebel.Service.Abstraction.File;
+using ArkoMebel.Service.Common.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ArkoMebel.Service.Service.Files
+{
+    public class FileService : IFileService
+    {
+        private readonly string MEDIA = "media";
+        private readonly string IMAGES = "images";
+       
+        private readonly string ROOTPATH;
+        public FileService(IWebHostEnvironment env)
+        {
+            ROOTPATH = env.WebRootPath;
+        }
+
+        public async ValueTask<bool> DeleteImageAsync(string file)
+        {
+            string path = Path.Combine(ROOTPATH, file);
+
+            if (File.Exists(path))
+            {
+                await Task.Run(() =>
+                {
+                    File.Delete(path);
+                });
+                return true;
+            }
+            return false;
+        }
+
+        public async ValueTask<byte[]> GetImageAsync(string fileName)
+        {
+            string path = Path.Combine(ROOTPATH, fileName);
+            byte[] imageBytes = await File.ReadAllBytesAsync(path);
+            return imageBytes;
+        }
+
+        public async ValueTask<string> UploadImageAsync(IFormFile file)
+        {
+            string newImageName = MediaHelper.MakeImageName(file.FileName.ToLower());
+            string subPath = Path.Combine(MEDIA, IMAGES, newImageName);
+            string path = Path.Combine(ROOTPATH, subPath);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+                return subPath;
+            }
+        }
+    }
+}
