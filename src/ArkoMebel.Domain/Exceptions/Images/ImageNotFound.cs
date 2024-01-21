f00014908f86281e5f6d0ab67577ddae2f87f425
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-21 17:06:40 +0500

    Exceptions

---
+++ src/ArkoMebel.Domain/Exceptions/Images/ImageNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace ArkoMebel.Domain.Exceptions.Images
+{
+    public class ImageNotFound : GlobalExceptions
+    {
+        public ImageNotFound()
+        {
+            TitleMessage = "Image Not Found";
+        }
+    }
+}
---
+++ src/ArkoMebel.Domain/Exceptions/PhoneNumber/PhoneNumberAlreadyExists.cs
@@ -0,0 +1,10 @@
+﻿namespace ArkoMebel.Domain.Exceptions.PhoneNumber
+{
+    public class PhoneNumberAlreadyExists:GlobalExceptions
+    {
+        public PhoneNumberAlreadyExists()
+        {
+            TitleMessage = "Phone Number Already Exists";
+        }
+    }
+}
