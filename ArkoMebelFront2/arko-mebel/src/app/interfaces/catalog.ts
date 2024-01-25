720aff62fb170e91d68ff90c3bd69655302b704f
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-25 15:31:54 +0500

    interfaces

---
+++ ArkoMebelFront2/arko-mebel/src/app/interfaces/catalog.ts
@@ -0,0 +1,6 @@
+
+export interface Catalog {
+    id:number;
+    name:string;
+    photoPath: string;
+}
---
+++ ArkoMebelFront2/arko-mebel/src/app/interfaces/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    id: number;
+    name: string;
+    description?: string;
+    imagePath: string;
+    price?: number;
+    catalogId?: number;
+}
