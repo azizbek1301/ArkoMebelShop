684894f0444d8d96a369c39bfbd977cd10677116
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-26 18:21:17 +0500

    Admin Model

---
+++ src/ArkoMebel.Domain/Entites/Admin.cs
@@ -0,0 +1,15 @@
+﻿using ArkoMebel.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArkoMebel.Domain.Entites
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public Role Role { get; set; }
+    }
+}
