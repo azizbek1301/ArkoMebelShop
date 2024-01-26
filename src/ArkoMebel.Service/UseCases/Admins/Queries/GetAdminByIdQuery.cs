76aa197ec7a4cd37b4cf2674c64d17a0d2a135b4
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-26 17:31:22 +0500

    Admin Queries

---
+++ src/ArkoMebel.Service/UseCases/Admins/Queries/GetAdminByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using ArkoMebel.Domain.Entites;
+using MediatR;
+
+namespace ArkoMebel.Service.UseCases.Admins.Queries
+{
+    public class GetAdminByIdQuery:IRequest<Admin>
+    {
+        public int AdminId {  get; set; }
+    }
+}
---
+++ src/ArkoMebel.Service/UseCases/Admins/Queries/GetAllAdminQuery.cs
@@ -0,0 +1,9 @@
+﻿using ArkoMebel.Domain.Entites;
+using MediatR;
+
+namespace ArkoMebel.Service.UseCases.Admins.Queries
+{
+    public class GetAllAdminQuery:IRequest<List<Admin>>
+    {
+    }
+}
