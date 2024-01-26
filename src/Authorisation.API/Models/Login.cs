dcdc28aa71e5dbd88c3c029621b1b751faff1b8b
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-26 17:29:26 +0500

    Login models

---
+++ src/Authorisation.API/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using Authorisation.API.Enums;
+using System.Text.Json.Serialization;
+
+namespace Authorisation.API.Models
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [JsonIgnore]
+        public Role Role { get; set; }
+    }
+}
