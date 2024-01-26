a1e15d39575eac86a51fc219d9637803d450b372
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-26 17:19:30 +0500

    Authorize Service

---
+++ src/Authorisation.API/Services/AuthService.cs
@@ -0,0 +1,53 @@
+﻿using ArkoMebel.Infrastructure.Peristance;
+using Authorisation.API.Interfaces;
+using Authorisation.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace Authorisation.API.Services
+{
+    public class AuthService: IAuthService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public async ValueTask<string> GenerateToken(Login login)
+        {
+            var admins = await _context.Admins.AsNoTracking().ToListAsync();
+
+            var admin = admins.FirstOrDefault(x => x.Email == login.Email && x.PasswordHash == PasswordHash.ComputeSHA512HashFromString(login.Password));
+
+           
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, admin.Email),
+                new Claim(ClaimTypes.Role, admin.Role.ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, EpochTime.GetIntDate(DateTime.UtcNow).ToString(CultureInfo.InvariantCulture), ClaimValueTypes.Integer64)
+
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddSeconds(10),
+                signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
