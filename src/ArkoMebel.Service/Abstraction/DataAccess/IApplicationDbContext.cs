00702e0fb7e7d3597e5540ffc26c64ab5ce3203c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-26 18:05:36 +0500

    ArkoMebel Abstractions

--- src/ArkoMebel.Service/Abstraction/DataAccess/IApplicationDbContext.cs
+++ src/ArkoMebel.Service/Abstraction/DataAccess/IApplicationDbContext.cs
@@ -17,6 +17,7 @@ namespace ArkoMebel.Service.Abstraction.DataAccess
         public DbSet<Prodact_Box> Prodact_Boxes { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
 
         public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
