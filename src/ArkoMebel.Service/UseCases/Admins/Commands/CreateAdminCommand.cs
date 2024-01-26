8dc997859f273c84381d22f164662d853a25e168
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-01-26 18:05:18 +0500

    Admin Commands

---
+++ src/ArkoMebel.Service/UseCases/Admins/Commands/CreateAdminCommand.cs
@@ -0,0 +1,9 @@
+﻿using ArkoMebel.Domain.Dtos.Admins;
+using MediatR;
+
+namespace ArkoMebel.Service.UseCases.Admins.Commands
+{
+    public class CreateAdminCommand:CreateAdminDto,IRequest<int>
+    {
+    }
+}
---
+++ src/ArkoMebel.Service/UseCases/Admins/Commands/DeleteAdminCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ArkoMebel.Service.UseCases.Admins.Commands
+{
+    public class DeleteAdminCommand:IRequest<int>
+    {
+        public int AdminId {  get; set; }
+    }
+}
